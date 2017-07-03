fdb18ae0424e58f96135f97024a18654e259d711
Randolph Pe Benito <rpebenito@xyber.ph>
2017-07-03 23:27:38 +0800

    Added Dynamic Routing.

---
+++ src/app/video-list/video-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-list',
+  templateUrl: './video-list.component.html',
+  styleUrls: ['./video-list.component.css']
+})
+export class VideoListComponent implements OnInit {
+	title = "Video List";
+	videoList = [
+		{
+		 name: "Item 1",
+		 slug: "item-1"   
+		},
+		{
+		 name: "Item 2",
+		 slug: "item-2"   
+		},
+		{
+		 name: "Item 3",
+		 slug: "item-3"   
+		}
+	];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

ad236c9a13995bf3cc92c387075aaa84c4448686
Randolph Pe Benito <rpebenito@xyber.ph>
2017-07-10 22:42:56 +0800

    Added angular-google-maps api library.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  zoom = 10;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  markers: marker[] = [
+    {
+      name: 'Company One',
+      lat: 51.688418,
+      lng: 7.909007,
+      draggable: true,
+    }, {
+      name: 'Company Two',
+      lat: 51.708418,
+      lng: 7.889007,
+      draggable: true,
+    }, {
+      name: 'Company Three',
+      lat: 51.610111,
+      lng: 7.849007,
+      draggable: false,
+    }
+  ];
+
+  constructor() {}
+
 }
+
+interface marker {
+  name?: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AgmCoreModule } from '@agm/core';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { VideoListComponent } from './video-list/video-list.component';
@@ -19,6 +21,9 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCOCBPqQyVaaVrSPwjadJYCuG3Dt-nhQaM'
+    }),
     AppRoutingModule
   ],
   providers: [],
